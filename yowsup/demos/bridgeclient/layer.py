4b4d41009c9b70742721727580544eba8811c187
Yannic Schneider <cyn@pride.vendetta.ch>
2018-11-02 09:03:12 +0100

    Added bridge functionality to telegram

---
+++ yowsup/demos/bridgeclient/layer.py
@@ -0,0 +1,38 @@
+import subprocess
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+
+class BridgeLayer(YowInterfaceLayer):
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+
+        if messageProtocolEntity.getType() == 'text':
+            self.onTextMessage(messageProtocolEntity)
+        elif messageProtocolEntity.getType() == 'media':
+            self.onMediaMessage(messageProtocolEntity)
+
+        #self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
+        self.toLower(messageProtocolEntity.ack())
+        self.toLower(messageProtocolEntity.ack(True))
+
+
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        self.toLower(entity.ack())
+
+    def onTextMessage(self,messageProtocolEntity):
+        # just print info
+        msg = messageProtocolEntity.getBody().encode('ascii', 'ignore')
+        print("Echoing %s to %s" % (msg, messageProtocolEntity.getFrom(False)))
+        subprocess.Popen(['/home/cyn/code/cyneye/thess.sh "%s"' %  msg], shell = True)
+
+    def onMediaMessage(self, messageProtocolEntity):
+        # just print info
+        if messageProtocolEntity.getMediaType() == "image":
+            print("Echoing image %s to %s" % (messageProtocolEntity.url, messageProtocolEntity.getFrom(False)))
+
+        elif messageProtocolEntity.getMediaType() == "location":
+            print("Echoing location (%s, %s) to %s" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
+
+        elif messageProtocolEntity.getMediaType() == "vcard":
+            print("Echoing vcard (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
